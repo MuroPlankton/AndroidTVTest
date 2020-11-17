6acaea2974e6bcc3fa3c69f558938e7a295b042c
MuroPlankton <73102721+MuroPlankton@users.noreply.github.com>
2020-11-17 17:04:31 +0200

    Basic app
    The app now launches and shows an image with some text.

---
+++ app/src/main/java/com/choicely/myapplication/TVActivity.java
@@ -0,0 +1,22 @@
+package com.choicely.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+public class TVActivity extends Activity {
+    private TextView textView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.activity_main_text);
+        imageView = findViewById(R.id.activity_main_image);
+    }
+}
