f42ac7e01a26f69f159cc19aaeac038571c6235f
BC\s1800861 <richardo.nykanen@gmail.com>
2020-11-18 12:23:20 +0200

    no message

--- app/src/main/java/com/choicely/myapplication/MainActivity.java
+++ app/src/main/java/com/choicely/myapplication/MainActivity.java
@@ -2,21 +2,32 @@ package com.choicely.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
 
-public class MainActivity extends Activity {
-    private TextView textView;
-    private ImageView imageView;
+public class MainActivity extends FragmentActivity {
+    private Button button1;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
 
-        textView = findViewById(R.id.activity_main_text);
-        imageView = findViewById(R.id.activity_main_image);
+        button1 = findViewById(R.id.main_activity_fragment_button_1);
+        button2 = findViewById(R.id.main_activity_fragment_button_2);
+        button3 = findViewById(R.id.main_activity_fragment_button_3);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
