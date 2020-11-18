79ed59046c2fc80abc427cdd66f44d9c83926629
BC\s1800861 <richardo.nykanen@gmail.com>
2020-11-18 15:12:02 +0200

    no message

--- app/src/main/java/com/choicely/myapplication/ThirdFragment.java
+++ app/src/main/java/com/choicely/myapplication/ThirdFragment.java
@@ -9,11 +9,13 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class ThirdFragment extends Fragment {
+    private View view;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.third_fragment, container, false);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.third_fragment, container, false);
+        return view;
     }
 }
